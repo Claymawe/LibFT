6f3e165eee1246bc52c3c4cedcb7a4356da317c6
Claymawe <90435482+Claymawe@users.noreply.github.com>
2021-09-15 12:42:26 +0930

    Add files via upload
    added new files

---
+++ Libft/ft_strtrim.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrobinso <mrobinso@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/15 12:01:18 by mrobinso          #+#    #+#             */
+/*   Updated: 2021/09/15 12:08:20 by mrobinso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+** Allocates with malloc(3) add returns a copy of s1 with the character specified
+**in set removed from the beginning and the end of the string.
+*/
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	return (0);
+}
--- Libft/libft.h
+++ Libft/libft.h
@@ -6,8 +6,61 @@
 /*   By: mrobinso <mrobinso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/10 10:34:31 by mrobinso          #+#    #+#             */
-/*   Updated: 2021/09/10 14:54:28 by mrobinso         ###   ########.fr       */
+/*   Updated: 2021/09/15 11:18:36 by mrobinso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "stdlib.h"
+#ifndef LIBFT_H
+# define LIBFT_H
+# include "stdlib.h"
+# include "stdlib.h"
+# include "unistd.h"
+
+void				*ft_memset(void *b, int c, size_t len);
+void				ft_bzero(void *s, size_t n);
+void				*ft_calloc(size_t count, size_t size);
+void				*ft_memcpy(void *dst, const void *src, size_t n);
+void				*ft_memchr(const void *s, int c, size_t n);
+int					ft_memcmp(const void *s1, const void *s2, size_t n);
+size_t				ft_strlent(const char *s);
+char				*ft_strdup(const char *s1);
+size_t				ft_strlcat(char *dst, const char *src, size_t size);
+char				*ft_strchr(const char *s, int c);
+char				*ft_strrchr(const char *s, int c);
+int					ft_strncmp(const char *s1, const char *s2, size_t n);
+int					ft_atoi(const char *str);
+int					ft_isalpha(int c);
+int					ft_isdigit(int c);
+int					ft_isalnum(int c);
+int					ft_isascii(int c);
+int					ft_isprint(int c);
+int					ft_toupper(int c);
+int					ft_tolower(int c);
+char				*ft_strnstr(const char *s1, const char *s2, size_t n);
+char				*ft_itoa(int n);
+void				ft_putchr_fd(char c, int fd);
+void				ft_putstr_fd(char *s, int fd);
+void				ft_putendl_fd(char *s, int fd);
+void				ft_putnbr_fd(int n, int fd);
+char				**ft_split(char const *s, char c);
+char				*ft_strtrim(char const *s1, char const *set);
+char				*ft_strjoin(char const *s1, char const *s2);
+char				*ft_substr(char const *s, unsigned int start, size_t len);
+void				ft_striteri(char *s, void (*f)(unsigned int, char));
+size_t				ft_strlcpy(char *dest, const char *src, size_t size);
+typedef struct s_list
+{
+	void			*context;
+	struct s_list	*next;
+}					t_list;
+t_list				*ft_lstnew(void *content);
+void				ft_istadd_front(t_list **lst, t_list *new);
+int					ft_lstsize(t_list *lst);
+t_list				*ft_lstlast(t_list *lst);
+void				ft_lstadd_back(t_list **lst, t_list *new);
+void				ft_lstdelone(t_list *lst, void (*del)(void *));
+void				ft_lstclear(t_list **lst, void (*del)(void *));
+void				ft_lstiter(t_list *lst, void (*f)(void *));
+t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
+						void (*del)(void *));
+#endif
